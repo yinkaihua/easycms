371298448bed1fcbc8a12267c34b93d388545ef2
yinkaihua <373679030@qq.com>
2016-01-23 21:58:32 +0800

    mybatis

---
+++ src/main/java/com/coding17/easycms/soa/entity/channel/TChannel.java
@@ -0,0 +1,65 @@
+package com.coding17.easycms.soa.entity.channel;
+
+import java.util.Date;
+
+public class TChannel {
+    private Integer id;
+
+    private String path;
+
+    private String name;
+
+    private Integer pid;
+
+    private Date createTime;
+
+    private Byte state;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path == null ? null : path.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Byte getState() {
+        return state;
+    }
+
+    public void setState(Byte state) {
+        this.state = state;
+    }
+}
---
+++ src/test/java/com/coding17/easycms/test/channel/ChannelTest.java
@@ -0,0 +1,40 @@
+package com.coding17.easycms.test.channel;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.coding17.easycms.soa.entity.channel.TChannel;
+
+public class ChannelTest {
+
+	private BeanFactory factory;
+	
+	private SqlSessionTemplate template;
+	
+	@Before
+	public void before() {
+		factory = new ClassPathXmlApplicationContext("spring-mybatis-test.xml");
+		template = (SqlSessionTemplate) factory.getBean("sqlSessionTemplate");
+	}
+	
+	@After
+	public void after() {
+		
+	}
+	
+	@Test
+	public void create() {
+		TChannel c = new TChannel();
+		c.setName("书记");
+		c.setPath("/sj");
+		c.setCreateTime(new Date());
+		template.insert("TChannelMapper.insert", c);
+	}
+	
+}
