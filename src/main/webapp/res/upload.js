c7e0a9b02ed9efd0676a94856f94b8f3cd0c9a18
yinkaihua <373679030@qq.com>
2016-03-11 19:13:14 +0800

    a

--- src/main/webapp/res/upload.js
+++ src/main/webapp/res/upload.js
@@ -1,18 +1,78 @@
-if (typeof kanglaohui=='undefined') {
+if (typeof $=='undefined') {
+	throw 'Need JQuery.';
+}
+
+kanglaohui = {};
+kanglaohui.widgets = {};
+kanglaohui.widgets.upload = function(selector, container, config) {
+	this.selectorCheck(selector);
+	this.setAttribute('action', config);
+//	this.
+};
+kanglaohui.widgets.upload.prototype = {
+	selectorCheck : function(selector) {
+		if ($(selector).attr('type')!='file') {
+			throw '绑定的元素不是input[file].';
+		}
+	},
+	setAttribute : function(attr, config) {
+		if (!config[attr]) {
+			throw attr+'未指定.';
+		}
+		this[attr] = config[attr];
+	},
+	bind : function(container) {
+		var formHtml = '<form method="post" enctype="multipart/form-data" action="'+this.action+'">';
+		formHtml+=$(container).html()+'</form>';
+		
+	}
+};
+
+/*if (typeof kanglaohui=='undefined') {
 	kanglaohui = new Object();
-	kanglaohui.addModule = function(name, module) {
+	kanglaohui.addModule = function(name, construct) {
 		if (!name) {
-			throw "module name is empty.";
+			throw 'module name is empty.';
 		}
 		if (!kanglaohui[name]) {
-			kanglaohui[name] = module;
+			kanglaohui[name] = construct;
 		}
 	}
 }
-kanglaohui.addModule('widgets.upload', {
+kanglaohui.addModule('widgets.upload', function(id) {
+	
+});
+kanglaohui['widgets.upload'].prototype = {
+	bind : function(selector, config) {
+		this.selectorCheck(selector);
+		this.setAttribute('action', config);
+	},
+	selectorCheck : function(selector) {
+		if ($('selector').attr('type')!='file') {
+			throw '绑定的元素不是input[file].';
+		}
+	},
+	setAttribute : function(attr, config) {
+		if (!config[attr]) {
+			throw attr+'未指定.';
+		}
+	}
+};*/
+/*kanglaohui.addModule('widgets.upload', {
 	//组件绑定
 	bind : function(selector, config) {
-		
+		this.selectorCheck(selector);
+		this.setAttribute('action', config);
+	},
+	selectorCheck : function(selector) {
+		if ($('selector').attr('type')!='file') {
+			throw '绑定的元素不是input[file].';
+		}
+	},
+	setAttribute : function(attr, config) {
+		if (!config[attr]) {
+			throw attr+'未指定.';
+		}
 	}
 });
-kanglaohui.addModule('widgets.dialog', {});
+kanglaohui.addModule('widgets.dialog', {});*/
